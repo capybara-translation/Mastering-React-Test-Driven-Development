1f202448b2a574f7f6a1a915f74caefffffb4f93
Capybara Translation <capybara.translation@gmail.com>
2019-11-10 15:50:57 +0900

    Introduced customer variable.

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -5,12 +5,21 @@ import { Appointment } from '../src/Appointment';
 describe('Appointment', () => {
   it('renders the customer first name', () => {
     const customer = { firstName: 'Ashley' };
-    const component = <Appointment customer={customer} />;
     const container = document.createElement('div');
-    document.body.appendChild(container);
+    ReactDOM.render(
+      <Appointment customer={customer} />,
+      container
+    );
+    expect(container.textContent).toMatch('Ashley');
+  });
 
-    ReactDOM.render(component, container);
-
-    expect(document.body.textContent).toMatch('Ashley');
+  it('renders another customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const container = document.createElement('div');
+    ReactDOM.render(
+      <Appointment customer={customer} />,
+      container
+    );
+    expect(container.textContent).toMatch('Jordan');
   });
 });
