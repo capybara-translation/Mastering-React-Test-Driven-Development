c339b0541c19d12b61a6ed5e131e6d8fbb94d532
Capybara Translation <capybara.translation@gmail.com>
2020-01-03 12:17:22 +0900

    Updated shallowHelpers.js

--- appointments/test/shallowHelpers.js
+++ appointments/test/shallowHelpers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 export const childrenOf = element => {
   if (typeof element === 'string') {
@@ -19,3 +20,40 @@ export const childrenOf = element => {
 
   return [children];
 };
+
+const elementsMatching = (element, matcherFn) => {
+  if (matcherFn(element)) {
+    return [element];
+  }
+  return childrenOf(element).reduce(
+    (acc, child) => [
+      ...acc,
+      ...elementsMatching(child, matcherFn)
+    ],
+    []
+  );
+};
+
+export const createShallowRenderer = () => {
+  let renderer = new ShallowRenderer();
+
+  return {
+    render: component => renderer.render(component),
+    elementMatching: matcherFn =>
+      elementsMatching(renderer.getRenderOutput(), matcherFn)[0],
+    elementsMatching: matcherFn =>
+      elementsMatching(renderer.getRenderOutput(), matcherFn),
+    child: n => childrenOf(renderer.getRenderOutput())[n]
+  };
+};
+
+export const type = typeName => element =>
+  element.type === typeName;
+
+export const id = id => element =>
+  element.props && element.props.id === id;
+
+export const className = className => element =>
+  element.props.className === className;
+
+export const click = element => element.props.onClick();
