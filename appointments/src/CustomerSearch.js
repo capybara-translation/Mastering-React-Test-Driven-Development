b8bff485da9ee6cb08c85708ff908a8a5bd368e4
Capybara Translation <capybara.translation@gmail.com>
2020-01-04 20:27:58 +0900

    - has a next button - requests next page of data when next button is clicked - displays next page of data when next button is clicked - has a previous button - moves back to first page when previous button is clicked - moves back one page when clicking previous after multiple clicks of the next button - moves back multiple pages

--- appointments/src/CustomerSearch.js
+++ appointments/src/CustomerSearch.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 const CustomerRow = ({ customer }) => (
   <tr>
@@ -9,37 +9,74 @@ const CustomerRow = ({ customer }) => (
   </tr>
 );
 
+const SearchButtons = ({ handleNext, handlePrevious }) => (
+  <div className="button-bar">
+    <button
+      role="button"
+      id="previous-page"
+      onClick={handlePrevious}>
+      Previous
+    </button>
+    <button role="button" id="next-page" onClick={handleNext}>
+      Next
+    </button>
+  </div>
+);
+
 export const CustomerSearch = () => {
   const [customers, setCustomers] = useState([]);
+  const [queryStrings, setQueryStrings] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await window.fetch('/customers', {
-        method: 'GET',
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json' }
-      });
+      let queryString = '';
+      if (queryStrings.length > 0)
+        queryString = queryStrings[queryStrings.length - 1];
+      const result = await window.fetch(
+        `/customers${queryString}`,
+        {
+          method: 'GET',
+          credentials: 'same-origin',
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
       setCustomers(await result.json());
     };
 
     fetchData();
-  }, []);
+  }, [queryStrings]);
+
+  const handleNext = useCallback(() => {
+    const after = customers[customers.length - 1].id;
+    const queryString = `?after=${after}`;
+    setQueryStrings([...queryStrings, queryString]);
+  }, [customers, queryStrings]);
+
+  const handlePrevious = useCallback(async () => {
+    setQueryStrings(queryStrings.slice(0, -1));
+  }, [queryStrings]);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Phone number</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customers.map(customer => (
-          <CustomerRow customer={customer} key={customer.id} />
-        ))}
-      </tbody>
-    </table>
+    <React.Fragment>
+      <SearchButtons
+        handleNext={handleNext}
+        handlePrevious={handlePrevious}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Phone number</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map(customer => (
+            <CustomerRow customer={customer} key={customer.id} />
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
   );
 };
